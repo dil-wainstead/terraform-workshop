3245763670ad7e7034545f994d4dcc4f7f4cef96
Steve Wainstead <spawlowski@diligent.com>
2021-12-21 14:19:28 -0500

    Missed a change from two commits ago The file wasn't saved, oops

--- 0000-setup/main.tf
+++ 0000-setup/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "exercise_0000" {
+resource "aws_instance" "sw_exercise_0000" {
   ami           = "ami-07ebfd5b3428b6f4d"
   instance_type = "t2.micro"
 
