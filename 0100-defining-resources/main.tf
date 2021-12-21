9a59e6aadfe31639fa369895b23b3e5be809f8a4
Steve Wainstead <spawlowski@diligent.com>
2021-12-21 14:15:57 -0500

    Changes made during the workshop

--- 0100-defining-resources/main.tf
+++ 0100-defining-resources/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "<INSERT_YOUR_AWS_ACCESS_KEY_HERE>"
-  secret_key = "<INSERT_YOUR_AWS_SECRET_KEY_HERE>"
-  region     = "us-east-1"
+  # access_key = "ASIAXVXZ7PU5S4ZGAWEO"
+  # secret_key = "ghEhR8bC2dO0AGhYxEWioAfwmMCBP6v8vXTyPthm"
+  # region     = "us-east-1"
 }
 
 # TODO
@@ -14,3 +14,13 @@ provider "aws" {
 # Be sure to tag it with:
 # - "Name" to "exercise_0010"
 # - "Terraform" to true
+resource "aws_instance" "sw_exercise_0100" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name      = "sw_exercise_0100"
+    Terraform = true
+    Ooga      = "booga"
+  }
+}
--- 0200-references/main.tf
+++ 0200-references/main.tf
@@ -2,6 +2,26 @@
 # 𝖴𝖲𝖤𝖲 𝖣𝖤𝖥𝖠𝖴𝖫𝖳 𝖵𝖯𝖢 !!!
 ########################
 
+variable "port" {
+  description = "This is the port number we use"
+  type        = number
+  default     = 443
+}
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_security_group" "sec_ssh" {
   # you can have as many ingress/egress blocks as you want
   ingress {
@@ -24,6 +44,58 @@ resource "aws_security_group" "sec_ssh" {
   }
 }
 
+resource "aws_security_group" "sec_http" {
+  # you can have as many ingress/egress blocks as you want
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "sec_http"
+    Terraform = true
+  }
+}
+
+resource "aws_instance" "sw_exercise_0200" {
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.sec_http.id,
+    aws_security_group.sec_ssh.id
+  ]
+
+  tags = {
+    Name      = "sw_exercise_0200"
+    Terraform = true
+    Ooga      = "booga"
+  }
+}
+
+output "example-ip" {
+  value       = aws_instance.sw_exercise_0200.public_ip
+  description = "The public IP address of our demo server"
+}
+
+output "ami_id" {
+  value       = data.aws_ami.latest_ubuntu.id
+  description = "The image ID we interrogated for the Ubuntu AMI"
+}
 # TODO
 # Introduce another security group (aws_security_group)
 # with the following constraints:
@@ -32,7 +104,7 @@ resource "aws_security_group" "sec_ssh" {
 #
 # TWO ingress rules
 #   1. from/to 8443 using tcp from CIDR blocks 192.168.0.0/16
-#   2. from/to 443 using tcp from CIDR blocks 192.168.0.0/16
+#   2.  from/to 443 using tcp from CIDR blocks 192.168.0.0/16
 #
 # ONE egress
 #   1. from/to 0 using tcp to 0.0.0.0/0
